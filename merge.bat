a775b29f571d3c2bd4e6c431cb0d99c5002f6b4e
Justin Murray <murrayju@addpcs.com>
2011-03-09 23:32:35 -0500

    Integrated CoreAudioApi and ZedGraph source code. Ready for beta release.

--- merge.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-ilmerge /target:winexe /out:SleepyTunes.exe SleepyTunes\bin\x86\Release\SleepyTunes.exe SleepyTunes\bin\x86\Release\CoreAudioApi.dll SleepyTunes\bin\x86\Release\ZedGraph.dll 
